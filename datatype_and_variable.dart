312eae9b425390dc5c85c1d76cbfaeffb33ff727
Rabi Bhagat <rabibhagat789@gmail.com>
2024-10-08 02:15:06 +0530

    Add files via upload

---
+++ datatype_and_variable.dart
@@ -0,0 +1,20 @@
+
+void main() {
+  // print('Be comeback');//this statment is printed
+  // stdout.write('Are You Ready for Comeback\n are you ready for the Best version of yourelf:');
+  // var name = stdin.readLineSync();
+  // print("be lonely because at a last you are only friend of yourself,$name");
+
+//declearation of variable.//
+
+  int a =3;
+
+  //(?) question mark = non reliable in dart meam
+  print(a);
+
+  // for var in tut(21)
+
+ 
+}
+
+// class Human {}
---
+++ object_class.dart
@@ -0,0 +1,9 @@
+void main() {
+  // print('mk');
+  // Human();
+  dynamic section;
+  section = "a";
+  section = 5;
+  section = true;
+  print(section);
+}
