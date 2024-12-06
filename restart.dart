cd99c8fa37ac2fa0c0273786244d9ea4fcba7b7e
Rabi Bhagat <rabibhagat789@gmail.com>
2024-12-06 23:42:14 +0530

    Add files via upload

---
+++ restart.dart
@@ -0,0 +1,8 @@
+void main() {
+  dynamic naya;//from here the dynamic section is starting from here
+  
+ naya = "i am on he restarting point :";
+ naya = "are you ready :";//here the it execute the second line because the dynamic holds the different types in different times.
+  print(naya);
+}
+
