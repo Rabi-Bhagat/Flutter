5ac43c379570ae3ebe234d5d9135d05025f88c9f
Rabi Bhagat <rabibhagat789@gmail.com>
2024-12-07 23:44:16 +0530

    Add files via upload

---
+++ without_dynamic.dart
@@ -0,0 +1,6 @@
+void main() {  
+var naya = "i am on he restarting point :";
+naya = "are you ready :";//here the it execute the second line because the dynamic holds the different types in different times.
+  print(naya);
+}
+
